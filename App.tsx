2699fec006b01ea0e075cd20de35b2b48be7d572
vn548ge <fahd.tahir@walmart.com>
2022-12-06 14:54:05 +0500

    Three flex boxes

--- App.tsx
+++ App.tsx
@@ -8,30 +8,44 @@ import {
 const App = () => {
 
   return (
-   <View style={styles.appContainer}>
-    <View style={styles.inputContainer}>
-      <TextInput style={styles.textInput} placeholder='Your course goal'/>
-      <Button title='Add Goal'/>
+    <View style={styles.appContainer}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+        <Text>3</Text>
+      </View>
     </View>
-    <View>
-      <Text>List of Goals!</Text>
-    </View>
-   </View>
   );
 };
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-  },
-  textInput: {
-    borderRadius: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
+    padding: 20
   },
 });
 
