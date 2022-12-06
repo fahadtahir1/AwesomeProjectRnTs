d8401c2ee1c13b065ed038490391fabea190cc16
vn548ge <fahd.tahir@walmart.com>
2022-12-06 15:34:23 +0500

    Kickstart Mock Goals App

--- App.tsx
+++ App.tsx
@@ -9,33 +9,13 @@ const App = () => {
 
   return (
     <View style={styles.appContainer}>
-      <View
-        style={{
-          backgroundColor: 'red',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-        <Text>1</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'blue',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-        <Text>3</Text>
-      </View>
+     <View style={styles.inputContainer}>
+      <TextInput style={styles.textInput} placeholder='Enter course goal'/>
+      <Button title='Add Goal'/>
+     </View>
+     <View>
+      <Text>List of Goals</Text>
+     </View>
     </View>
   );
 };
@@ -43,11 +23,15 @@ const App = () => {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 20,
+  },
+  inputContainer:{
     flexDirection: 'row',
-    width: '80%',
-    height: 300,
-    justifyContent: 'space-between',
-    alignItems: 'stretch',
+    marginRight: 10,
+  },
+  textInput: {
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    width: '70%',
   },
 });
 
